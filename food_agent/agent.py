ca2cc71284ff98f2a73a6cc69e14fa84014184cb
rafehr <rafael.e2311@gmail.com>
2026-03-18 20:07:32 +0100

    Rename chat node

--- food_agent/agent.py
+++ food_agent/agent.py
@@ -2,7 +2,7 @@ from langchain_core.messages import HumanMessage
 from langgraph.graph import END, START, StateGraph
 
 from food_agent.utils.nodes import (
-    chat,
+    chat_node,
     error_handling,
     input_classifier,
     input_router,
@@ -21,32 +21,29 @@ builder.add_node("validate_refinement", validate_refinement)
 builder.add_node("query_db", query_db)
 builder.add_node("error_handling", error_handling)
 builder.add_node("input_classifier", input_classifier)
-builder.add_node("chat", chat)
+builder.add_node("chat_node", chat_node)
 
-
-builder.add_edge(START, "refine_query")
-builder.add_edge("refine_query", "validate_refinement")
 builder.add_edge(START, "input_classifier")
 builder.add_conditional_edges(
-    "input_classifier", input_router, {"query": "refine_query", "no_query": "chat"}
+    "input_classifier", input_router, {"query": "refine_query", "no_query": "chat_node"}
 )
+builder.add_edge("refine_query", "validate_refinement")
 builder.add_conditional_edges(
     "validate_refinement",
     refinement_router,
     {"retry": "refine_query", "query_db": "query_db", "give_up": "error_handling"},
 )
 builder.add_edge("query_db", END)
-builder.add_edge("chat", END)
+builder.add_edge("chat_node", END)
 
 query = """Hey, how are you! I'm talking to a Chatbot for the first time.
 I am not sure how this work, but here we go: I would like to give me results
 for Ice Cream with caramell. Maybe by Ben and Jerry's, but other brands are fine, too."""
+# query = "Hey, what is your name?"
 
 agent = builder.compile()
 
 draw_graph(agent)
-
-exit()
 
 inputs: GraphState = {
     "messages": [HumanMessage(content=query)],
@@ -55,5 +52,6 @@ inputs: GraphState = {
     "retrieved_products": [],
     "retry_count": 0,
     "error_log": None,
+    "is_query": False,
 }
 agent.invoke(inputs)
