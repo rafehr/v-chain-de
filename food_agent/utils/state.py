f2a7d106762d01c0684b82f263955e3b5f64d8a5
rafehr <rafael.e2311@gmail.com>
2026-03-19 12:50:26 +0100

    Add remaining steps

--- food_agent/utils/state.py
+++ food_agent/utils/state.py
@@ -2,6 +2,8 @@ from typing import Annotated, Dict, List, TypedDict
 
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
+from langgraph.managed import RemainingSteps
+from typing_extensions import NotRequired
 
 
 class GraphState(TypedDict):
@@ -12,3 +14,4 @@ class GraphState(TypedDict):
     retry_count: int
     error_log: str | None
     is_query: bool
+    remaining_steps: NotRequired[RemainingSteps]
