79b1cc4ce79549df853b1bd981a199b04f576272
rafehr <rafael.e2311@gmail.com>
2026-03-27 16:47:03 +0100

    Add claude model

--- food_agent/utils/models.py
+++ food_agent/utils/models.py
@@ -2,25 +2,57 @@ import os
 from functools import lru_cache
 
 from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_core.utils import convert_to_secret_str
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 load_dotenv()
 
-ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+HF_ACCESS_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+CLAUDE_ACCESS_TOKEN = os.getenv("CLAUDE_API_TOKEN")
 
 
 @lru_cache(maxsize=10)
 def get_model(
-    model_name: str, temperature: float, max_new_tokens: int = 4096
+    model_name: str,
+    temperature: float,
+    max_new_tokens: int = 4096,
+) -> ChatAnthropic:
+    """
+    Instantiates a model using HuggingFaceEndpoint.
+    """
+
+    if CLAUDE_ACCESS_TOKEN is None:
+        raise ValueError("CLAUDE_ACCESS_TOKEN not set.")
+
+    secret_key = convert_to_secret_str(CLAUDE_ACCESS_TOKEN)
+    llm = ChatAnthropic(
+        model_name=model_name,
+        temperature=0,
+        max_tokens_to_sample=1024,
+        timeout=None,
+        stop=None,
+        api_key=secret_key,
+    )
+    return llm
+
+
+@lru_cache(maxsize=10)
+def get_hf_model(
+    model_name: str,
+    temperature: float,
+    max_new_tokens: int = 4096,
+    json_output: bool = False,
 ) -> ChatHuggingFace:
     """
     Instantiates a model using HuggingFaceEndpoint.
     """
+
     llm = HuggingFaceEndpoint(
         model=model_name,
         task="conversational",
         max_new_tokens=max_new_tokens,
         temperature=temperature,
-        huggingfacehub_api_token=ACCESS_TOKEN,
+        huggingfacehub_api_token=HF_ACCESS_TOKEN,
     )
     return ChatHuggingFace(llm=llm)
